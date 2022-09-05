6e6bceb71ead6b8ecdfc88b61f0a930f786bf36f
unknown <ononobip@gmail.com>
2022-09-05 18:21:58 +0100

    HEHEHE

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Images from "./images";
 
 function App() {
+  const [selectedImg, setSelectedImg] = useState(
+    Images[Math.trunc(Math.random() * 9)]
+  );
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <img src={selectedImg} alt="" className="selected" />
+        <div className="imgContainer">
+          {Images.map((img, i) => (
+            <img
+              src={img}
+              key={i}
+              alt=""
+              onClick={() =>setSelectedImg(img)}
+              style={{ border: selectedImg === img ? "3px solid white" : "" }}
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/images.js
@@ -0,0 +1,11 @@
+const Images = [
+    "https://i.pinimg.com/564x/3d/ed/bd/3dedbd4081783734145fb894f4ccad28.jpg",
+    "https://i.pinimg.com/564x/a2/45/24/a24524c60d6ea16adf8cf6aad6b45e77.jpg",
+    "https://i.pinimg.com/564x/5d/0a/1f/5d0a1f1f5e97c1d2afb6e5ff95c8162d.jpg",
+    "https://i.pinimg.com/564x/91/18/b9/9118b947064c002781406ca301443ec2.jpg",
+    "https://i.pinimg.com/564x/77/6c/b4/776cb46e9e30b5a59411576d62d1af10.jpg",
+    "https://i.pinimg.com/564x/80/4b/f8/804bf8464b1c0f193577ccee653b8776.jpg",
+    "https://i.pinimg.com/564x/5e/30/5a/5e305a851bf1c4bcaf1284be3fe36c75.jpg",
+    "https://i.pinimg.com/564x/70/af/d3/70afd333d4efe2bdbaf84d7020f4cbf0.jpg",
+]
+export default Images;
